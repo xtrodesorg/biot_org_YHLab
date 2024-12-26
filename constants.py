e3496b6d755233e4540138d671514c7a46d49f38
AmielWexler1 <168630718+AmielWexler1@users.noreply.github.com>
2024-12-26 15:01:53 +0200

    revert to release

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 import os
 
-manufacturer_id = '00000000-0000-0000-0000-000000000000'
+#manufacturer_id = '00000000-0000-0000-0000-000000000000'
 PROD_BASE_URL = 'https://api.xtrodes.biot-med.com'
 DEV_BASE_URL = 'https://api.dev.xtrodes1.biot-med.com'
 
